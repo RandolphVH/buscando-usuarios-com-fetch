670b3c80cc6f97cc08a56239306384d1d0c88ea6
Victor Lima <victorhugolima.vh@gmail.com>
2026-04-03 22:20:26 -0300

    adiciona validação de entrada e mensagem de erro para busca de usuário

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,6 +5,7 @@ import { screen } from './objects/screen.js';
 
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value;
+    if(validateEmptyInput(userName)) return;
     getUserData(userName);
 })
 
@@ -12,15 +13,28 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
     const userName = e.target.value;
     const key = e.which || e.keyCode;
     const isEnterKeyPressed = key === 13;
-
+    
     if (isEnterKeyPressed) {
+        if(validateEmptyInput(userName)) return;
         getUserData(userName);
     }
 })
 
-async function getUserData(userName) {
+function validateEmptyInput(userName) {
+    if(userName.length === 0){
+        alert('Digite um nome de usuário do GitHub para realizar a busca');
+        return true;
+    }
+}
 
-    const userResponse = await getUser(userName)
+async function getUserData(userName) {
+    const userResponse = await getUser(userName);
+    
+    if(userResponse.message === 'Not Found') {
+        screen.renderNotFound();
+        return;
+    }
+    
     const repositoriesResponse = await getRepositories(userName);
     user.setInfo(userResponse);
     user.setRepositories(repositoriesResponse)
